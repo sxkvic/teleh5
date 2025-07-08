9ac9342dbe90586ea793a75f2480723fffd53d9b
施兴科 <shixingke@ovopark.com>
2025-07-08 10:14:22 +0800

    feat: 初始化Vue项目并添加基础配置和组件
    添加Vue项目基础文件结构，包括配置文件和静态资源
    引入Vant UI组件库并配置按需加载
    设置PostCSS和Babel相关配置
    添加.gitignore文件和基础README说明
    创建主应用入口和示例组件

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+html {
+  font-size: 16px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
